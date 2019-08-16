3c14b73564b755b89fe1106de632fa6e2378634e
Qa <jigadhirasu@gmail.com>
2019-08-16 10:22:44 +0800

    update

--- xgrpc/hello/hello.pb.go
+++ xgrpc/hello/hello.pb.go
@@ -26,7 +26,6 @@ const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
 type HelloTask struct {
 	Name                 string   `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
-	Message              string   `protobuf:"bytes,2,opt,name=message,proto3" json:"message,omitempty"`
 	Timestamp            int32    `protobuf:"varint,3,opt,name=timestamp,proto3" json:"timestamp,omitempty"`
 	Response             string   `protobuf:"bytes,4,opt,name=response,proto3" json:"response,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
@@ -66,13 +65,6 @@ func (m *HelloTask) GetName() string {
 	return ""
 }
 
-func (m *HelloTask) GetMessage() string {
-	if m != nil {
-		return m.Message
-	}
-	return ""
-}
-
 func (m *HelloTask) GetTimestamp() int32 {
 	if m != nil {
 		return m.Timestamp
@@ -94,18 +86,19 @@ func init() {
 func init() { proto.RegisterFile("hello.proto", fileDescriptor_61ef911816e0a8ce) }
 
 var fileDescriptor_61ef911816e0a8ce = []byte{
-	// 172 bytes of a gzipped FileDescriptorProto
+	// 177 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0xce, 0x48, 0xcd, 0xc9,
-	0xc9, 0xd7, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x05, 0x73, 0x94, 0x8a, 0xb9, 0x38, 0x3d,
+	0xc9, 0xd7, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x05, 0x73, 0x94, 0x22, 0xb9, 0x38, 0x3d,
 	0x40, 0x8c, 0x90, 0xc4, 0xe2, 0x6c, 0x21, 0x21, 0x2e, 0x96, 0xbc, 0xc4, 0xdc, 0x54, 0x09, 0x46,
-	0x05, 0x46, 0x0d, 0xce, 0x20, 0x30, 0x5b, 0x48, 0x82, 0x8b, 0x3d, 0x37, 0xb5, 0xb8, 0x38, 0x31,
-	0x3d, 0x55, 0x82, 0x09, 0x2c, 0x0c, 0xe3, 0x0a, 0xc9, 0x70, 0x71, 0x96, 0x64, 0xe6, 0xa6, 0x16,
-	0x97, 0x24, 0xe6, 0x16, 0x48, 0x30, 0x2b, 0x30, 0x6a, 0xb0, 0x06, 0x21, 0x04, 0x84, 0xa4, 0xb8,
-	0x38, 0x8a, 0x52, 0x8b, 0x0b, 0xf2, 0xf3, 0x8a, 0x53, 0x25, 0x58, 0xc0, 0x1a, 0xe1, 0x7c, 0xa3,
-	0x3c, 0x2e, 0x1e, 0xb0, 0xa5, 0xc1, 0xa9, 0x45, 0x65, 0x99, 0xc9, 0xa9, 0x42, 0x06, 0x5c, 0x1c,
-	0xc1, 0x89, 0x95, 0x60, 0x21, 0x21, 0x01, 0x3d, 0x88, 0x2b, 0xe1, 0xae, 0x92, 0xc2, 0x10, 0x31,
-	0x60, 0x14, 0xd2, 0xe1, 0x62, 0x8b, 0x20, 0x5a, 0x7d, 0x12, 0x1b, 0xd8, 0xcb, 0xc6, 0x80, 0x00,
-	0x00, 0x00, 0xff, 0xff, 0xd9, 0xce, 0xdc, 0xc6, 0x01, 0x01, 0x00, 0x00,
+	0x05, 0x46, 0x0d, 0xce, 0x20, 0x30, 0x5b, 0x48, 0x86, 0x8b, 0xb3, 0x24, 0x33, 0x37, 0xb5, 0xb8,
+	0x24, 0x31, 0xb7, 0x40, 0x82, 0x59, 0x81, 0x51, 0x83, 0x35, 0x08, 0x21, 0x20, 0x24, 0xc5, 0xc5,
+	0x51, 0x94, 0x5a, 0x5c, 0x90, 0x9f, 0x57, 0x9c, 0x2a, 0xc1, 0x02, 0xd6, 0x05, 0xe7, 0x1b, 0x3d,
+	0x62, 0xe4, 0xe2, 0x01, 0x9b, 0x1d, 0x9c, 0x5a, 0x54, 0x96, 0x99, 0x9c, 0x2a, 0x64, 0xcd, 0x25,
+	0x18, 0x5c, 0x52, 0x94, 0x9a, 0x98, 0x0b, 0x11, 0x05, 0x33, 0x85, 0x04, 0xf4, 0x20, 0xae, 0x82,
+	0xbb, 0x42, 0x0a, 0x43, 0x44, 0x83, 0xd1, 0x80, 0x51, 0xc8, 0x98, 0x8b, 0x1b, 0x49, 0x33, 0x71,
+	0xda, 0x40, 0x9a, 0x48, 0xb4, 0xcb, 0x80, 0x51, 0x48, 0x9b, 0x8b, 0x95, 0x68, 0x3b, 0x92, 0xd8,
+	0xc0, 0xa1, 0x69, 0x0c, 0x08, 0x00, 0x00, 0xff, 0xff, 0x34, 0xe0, 0xac, 0x44, 0x5c, 0x01, 0x00,
+	0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -120,8 +113,10 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type HelloServiceClient interface {
-	SayHello(ctx context.Context, in *HelloTask, opts ...grpc.CallOption) (HelloService_SayHelloClient, error)
-	XHello(ctx context.Context, in *HelloTask, opts ...grpc.CallOption) (*HelloTask, error)
+	StreamHelloStream(ctx context.Context, opts ...grpc.CallOption) (HelloService_StreamHelloStreamClient, error)
+	StreamHello(ctx context.Context, opts ...grpc.CallOption) (HelloService_StreamHelloClient, error)
+	HelloStream(ctx context.Context, in *HelloTask, opts ...grpc.CallOption) (HelloService_HelloStreamClient, error)
+	Hello(ctx context.Context, in *HelloTask, opts ...grpc.CallOption) (*HelloTask, error)
 }
 
 type helloServiceClient struct {
@@ -132,12 +127,77 @@ func NewHelloServiceClient(cc *grpc.ClientConn) HelloServiceClient {
 	return &helloServiceClient{cc}
 }
 
-func (c *helloServiceClient) SayHello(ctx context.Context, in *HelloTask, opts ...grpc.CallOption) (HelloService_SayHelloClient, error) {
-	stream, err := c.cc.NewStream(ctx, &_HelloService_serviceDesc.Streams[0], "/hello.HelloService/SayHello", opts...)
+func (c *helloServiceClient) StreamHelloStream(ctx context.Context, opts ...grpc.CallOption) (HelloService_StreamHelloStreamClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_HelloService_serviceDesc.Streams[0], "/hello.HelloService/StreamHelloStream", opts...)
 	if err != nil {
 		return nil, err
 	}
-	x := &helloServiceSayHelloClient{stream}
+	x := &helloServiceStreamHelloStreamClient{stream}
+	return x, nil
+}
+
+type HelloService_StreamHelloStreamClient interface {
+	Send(*HelloTask) error
+	Recv() (*HelloTask, error)
+	grpc.ClientStream
+}
+
+type helloServiceStreamHelloStreamClient struct {
+	grpc.ClientStream
+}
+
+func (x *helloServiceStreamHelloStreamClient) Send(m *HelloTask) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *helloServiceStreamHelloStreamClient) Recv() (*HelloTask, error) {
+	m := new(HelloTask)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (c *helloServiceClient) StreamHello(ctx context.Context, opts ...grpc.CallOption) (HelloService_StreamHelloClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_HelloService_serviceDesc.Streams[1], "/hello.HelloService/StreamHello", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &helloServiceStreamHelloClient{stream}
+	return x, nil
+}
+
+type HelloService_StreamHelloClient interface {
+	Send(*HelloTask) error
+	CloseAndRecv() (*HelloTask, error)
+	grpc.ClientStream
+}
+
+type helloServiceStreamHelloClient struct {
+	grpc.ClientStream
+}
+
+func (x *helloServiceStreamHelloClient) Send(m *HelloTask) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *helloServiceStreamHelloClient) CloseAndRecv() (*HelloTask, error) {
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	m := new(HelloTask)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (c *helloServiceClient) HelloStream(ctx context.Context, in *HelloTask, opts ...grpc.CallOption) (HelloService_HelloStreamClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_HelloService_serviceDesc.Streams[2], "/hello.HelloService/HelloStream", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &helloServiceHelloStreamClient{stream}
 	if err := x.ClientStream.SendMsg(in); err != nil {
 		return nil, err
 	}
@@ -147,16 +207,16 @@ func (c *helloServiceClient) SayHello(ctx context.Context, in *HelloTask, opts .
 	return x, nil
 }
 
-type HelloService_SayHelloClient interface {
+type HelloService_HelloStreamClient interface {
 	Recv() (*HelloTask, error)
 	grpc.ClientStream
 }
 
-type helloServiceSayHelloClient struct {
+type helloServiceHelloStreamClient struct {
 	grpc.ClientStream
 }
 
-func (x *helloServiceSayHelloClient) Recv() (*HelloTask, error) {
+func (x *helloServiceHelloStreamClient) Recv() (*HelloTask, error) {
 	m := new(HelloTask)
 	if err := x.ClientStream.RecvMsg(m); err != nil {
 		return nil, err
@@ -164,9 +224,9 @@ func (x *helloServiceSayHelloClient) Recv() (*HelloTask, error) {
 	return m, nil
 }
 
-func (c *helloServiceClient) XHello(ctx context.Context, in *HelloTask, opts ...grpc.CallOption) (*HelloTask, error) {
+func (c *helloServiceClient) Hello(ctx context.Context, in *HelloTask, opts ...grpc.CallOption) (*HelloTask, error) {
 	out := new(HelloTask)
-	err := c.cc.Invoke(ctx, "/hello.HelloService/XHello", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/hello.HelloService/Hello", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -175,60 +235,120 @@ func (c *helloServiceClient) XHello(ctx context.Context, in *HelloTask, opts ...
 
 // HelloServiceServer is the server API for HelloService service.
 type HelloServiceServer interface {
-	SayHello(*HelloTask, HelloService_SayHelloServer) error
-	XHello(context.Context, *HelloTask) (*HelloTask, error)
+	StreamHelloStream(HelloService_StreamHelloStreamServer) error
+	StreamHello(HelloService_StreamHelloServer) error
+	HelloStream(*HelloTask, HelloService_HelloStreamServer) error
+	Hello(context.Context, *HelloTask) (*HelloTask, error)
 }
 
 // UnimplementedHelloServiceServer can be embedded to have forward compatible implementations.
 type UnimplementedHelloServiceServer struct {
 }
 
-func (*UnimplementedHelloServiceServer) SayHello(req *HelloTask, srv HelloService_SayHelloServer) error {
-	return status.Errorf(codes.Unimplemented, "method SayHello not implemented")
+func (*UnimplementedHelloServiceServer) StreamHelloStream(srv HelloService_StreamHelloStreamServer) error {
+	return status.Errorf(codes.Unimplemented, "method StreamHelloStream not implemented")
 }
-func (*UnimplementedHelloServiceServer) XHello(ctx context.Context, req *HelloTask) (*HelloTask, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method XHello not implemented")
+func (*UnimplementedHelloServiceServer) StreamHello(srv HelloService_StreamHelloServer) error {
+	return status.Errorf(codes.Unimplemented, "method StreamHello not implemented")
+}
+func (*UnimplementedHelloServiceServer) HelloStream(req *HelloTask, srv HelloService_HelloStreamServer) error {
+	return status.Errorf(codes.Unimplemented, "method HelloStream not implemented")
+}
+func (*UnimplementedHelloServiceServer) Hello(ctx context.Context, req *HelloTask) (*HelloTask, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Hello not implemented")
 }
 
 func RegisterHelloServiceServer(s *grpc.Server, srv HelloServiceServer) {
 	s.RegisterService(&_HelloService_serviceDesc, srv)
 }
 
-func _HelloService_SayHello_Handler(srv interface{}, stream grpc.ServerStream) error {
+func _HelloService_StreamHelloStream_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(HelloServiceServer).StreamHelloStream(&helloServiceStreamHelloStreamServer{stream})
+}
+
+type HelloService_StreamHelloStreamServer interface {
+	Send(*HelloTask) error
+	Recv() (*HelloTask, error)
+	grpc.ServerStream
+}
+
+type helloServiceStreamHelloStreamServer struct {
+	grpc.ServerStream
+}
+
+func (x *helloServiceStreamHelloStreamServer) Send(m *HelloTask) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *helloServiceStreamHelloStreamServer) Recv() (*HelloTask, error) {
+	m := new(HelloTask)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func _HelloService_StreamHello_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(HelloServiceServer).StreamHello(&helloServiceStreamHelloServer{stream})
+}
+
+type HelloService_StreamHelloServer interface {
+	SendAndClose(*HelloTask) error
+	Recv() (*HelloTask, error)
+	grpc.ServerStream
+}
+
+type helloServiceStreamHelloServer struct {
+	grpc.ServerStream
+}
+
+func (x *helloServiceStreamHelloServer) SendAndClose(m *HelloTask) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *helloServiceStreamHelloServer) Recv() (*HelloTask, error) {
+	m := new(HelloTask)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func _HelloService_HelloStream_Handler(srv interface{}, stream grpc.ServerStream) error {
 	m := new(HelloTask)
 	if err := stream.RecvMsg(m); err != nil {
 		return err
 	}
-	return srv.(HelloServiceServer).SayHello(m, &helloServiceSayHelloServer{stream})
+	return srv.(HelloServiceServer).HelloStream(m, &helloServiceHelloStreamServer{stream})
 }
 
-type HelloService_SayHelloServer interface {
+type HelloService_HelloStreamServer interface {
 	Send(*HelloTask) error
 	grpc.ServerStream
 }
 
-type helloServiceSayHelloServer struct {
+type helloServiceHelloStreamServer struct {
 	grpc.ServerStream
 }
 
-func (x *helloServiceSayHelloServer) Send(m *HelloTask) error {
+func (x *helloServiceHelloStreamServer) Send(m *HelloTask) error {
 	return x.ServerStream.SendMsg(m)
 }
 
-func _HelloService_XHello_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _HelloService_Hello_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(HelloTask)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(HelloServiceServer).XHello(ctx, in)
+		return srv.(HelloServiceServer).Hello(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/hello.HelloService/XHello",
+		FullMethod: "/hello.HelloService/Hello",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(HelloServiceServer).XHello(ctx, req.(*HelloTask))
+		return srv.(HelloServiceServer).Hello(ctx, req.(*HelloTask))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -238,14 +358,25 @@ var _HelloService_serviceDesc = grpc.ServiceDesc{
 	HandlerType: (*HelloServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "XHello",
-			Handler:    _HelloService_XHello_Handler,
+			MethodName: "Hello",
+			Handler:    _HelloService_Hello_Handler,
 		},
 	},
 	Streams: []grpc.StreamDesc{
 		{
-			StreamName:    "SayHello",
-			Handler:       _HelloService_SayHello_Handler,
+			StreamName:    "StreamHelloStream",
+			Handler:       _HelloService_StreamHelloStream_Handler,
+			ServerStreams: true,
+			ClientStreams: true,
+		},
+		{
+			StreamName:    "StreamHello",
+			Handler:       _HelloService_StreamHello_Handler,
+			ClientStreams: true,
+		},
+		{
+			StreamName:    "HelloStream",
+			Handler:       _HelloService_HelloStream_Handler,
 			ServerStreams: true,
 		},
 	},
