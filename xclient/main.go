30af9a093f0c0deb51b562d626f04ad08a63901f
Qa <jigadhirasu@gmail.com>
2019-08-16 11:34:16 +0800

    456789

--- xclient/main.go
+++ xclient/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/jigadhirasu/laboratory/xgrpc/hello"
@@ -11,22 +12,53 @@ import (
 
 func main() {
 
-	conn, _ := grpc.Dial(":17888", grpc.WithInsecure())
+	log.SetFlags(log.Llongfile)
+
+	conn, _ := grpc.Dial(":17887", grpc.WithInsecure())
 	defer conn.Close()
 
 	client := hello.NewHelloServiceClient(conn)
 
-	stream, _ := client.SayHello(context.Background(), &hello.HelloTask{})
+	// 1to1
+	log.Println(client.Hello(context.Background(), &hello.HelloTask{Timestamp: int32(time.Now().Unix())}))
 
-	times := 0
-	for {
-		ht, _ := stream.Recv()
-		fmt.Printf("%#v \n", ht)
-		times++
-		if times > 10 {
-			break
-		}
-		<-time.After(time.Second)
+	// Sto1
+	stream1, err := client.StreamHello(context.Background())
+	if err != nil {
+		log.Println(err)
 	}
-	stream.CloseSend()
+
+	stream1.Send(&hello.HelloTask{})
+	stream1.Send(&hello.HelloTask{})
+	stream1.Send(&hello.HelloTask{})
+	h1 := &hello.HelloTask{}
+	stream1.RecvMsg(h1)
+	log.Println(h1)
+
+	// 1toS
+
+	stream2, err := client.HelloStream(context.Background(), &hello.HelloTask{Name: "ABC"})
+	if err != nil {
+		log.Println(err)
+	}
+	for i := 0; i < 3; i++ {
+		log.Println(stream2.Recv())
+	}
+	stream2.CloseSend()
+
+	// StoS
+
+	stream3, err := client.StreamHelloStream(context.Background())
+	for i := 0; i < 3; i++ {
+		stream3.Send(&hello.HelloTask{Name: fmt.Sprintf("ABC%d", i+1)})
+
+		ht, err := stream3.Recv()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println(ht)
+	}
+	stream3.CloseSend()
+
 }
